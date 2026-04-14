1a42dd5154fba5f884542074763aac5afe033d68
Annabelle McLean <ms5hc7vw7d@privaterelay.appleid.com>
2026-04-14 16:25:01 -0400

    middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,10 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({ request })
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    console.error('MISSING SUPABASE ENV VARS')
+    return NextResponse.next()  let supabaseResponse = NextResponse.next({ request })
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
